8a27d7409688adb136c79328e939c471acfbcb9d
LucasO24 <LucasOjeda2405@gmail.com>
2024-11-10 17:45:00 -0300

    Update Tema03_procedimientos_y_funciones_almacenadas.sql

--- script/Tema3_Procedimientos_y_funciones/Tema03_procedimientos_y_funciones_almacenadas.sql
+++ script/Tema3_Procedimientos_y_funciones/Tema03_procedimientos_y_funciones_almacenadas.sql
@@ -1,6 +1,5 @@
 --AGREGAR
 CREATE PROCEDURE InsertarProducto
-    @IdProducto int,
     @Codigo VARCHAR(50),
     @Nombre VARCHAR(50),
     @Descripcion VARCHAR(255),
@@ -10,12 +9,12 @@ CREATE PROCEDURE InsertarProducto
     @Categoria_id INT
 AS
 BEGIN
-    INSERT INTO Producto (Id_Producto, Codigo, Nombre, Descripcion, Stock, Precio, Estado, Categoria_id)
-    VALUES (@IdProducto, @Codigo, @Nombre, @Descripcion, @Stock, @Precio, @Estado, @Categoria_id);
+    INSERT INTO Producto (Codigo, Nombre, Descripcion, Stock, Precio, Estado, Categoria_id)
+    VALUES (@Codigo, @Nombre, @Descripcion, @Stock, @Precio, @Estado, @Categoria_id);
 END
 
 --EJEMPLO
-    EXECUTE InsertarProducto 1, 'c44', 'Arroz Danubio', 'Arroz Marca Danubio De 500grs', 45, 1500.00, 1, 4;
+    EXECUTE InsertarProducto 'c44', 'Arroz Danubio', 'Arroz Marca Danubio De 500grs', 45, 1500.00, 1, 4;
   
 --MODIFICAR
 --Agregar cantidad a un producto
@@ -54,15 +53,15 @@ END
 
 --Insertar un lote de datos en las tablas mencionadas  (guardar el script) con sentencias insert y otro lote invocando a los procedimientos creados. 
 
-INSERT INTO Producto (Id_Producto, Codigo, Nombre, Descripcion, Stock, Precio, Estado, Categoria_id) VALUES (1, 'A04', 'Lata Durazno', 'Durazno Enlatado', 49, 2430.00, 1, 3)
-INSERT INTO Producto (Id_Producto, Codigo, Nombre, Descripcion, Stock, Precio, Estado, Categoria_id) VALUES (2, 'B44', 'Azucar AG', 'Azucar de un kilogramo', 459, 2000.00, 1, 3)
-INSERT INTO Producto (Id_Producto, Codigo, Nombre, Descripcion, Stock, Precio, Estado, Categoria_id) VALUES (3, 'C09', 'Lata Durazno', 'Durazno Enlatado', 49, 2430.00, 1, 2)
-INSERT INTO Producto (Id_Producto, Codigo, Nombre, Descripcion, Stock, Precio, Estado, Categoria_id) VALUES (4, 'A10', 'Lata Durazno', 'Durazno Enlatado', 49, 2430.00, 1, 1)
+INSERT INTO Producto (Codigo, Nombre, Descripcion, Stock, Precio, Estado, Categoria_id) VALUES ('A04', 'Lata Durazno', 'Durazno Enlatado', 49, 2430.00, 1, 3)
+INSERT INTO Producto (Codigo, Nombre, Descripcion, Stock, Precio, Estado, Categoria_id) VALUES ('B44', 'Azucar AG', 'Azucar de un kilogramo', 459, 2000.00, 1, 3)
+INSERT INTO Producto (Codigo, Nombre, Descripcion, Stock, Precio, Estado, Categoria_id) VALUES ('C09', 'Lata Durazno', 'Durazno Enlatado', 49, 2430.00, 1, 2)
+INSERT INTO Producto (Codigo, Nombre, Descripcion, Stock, Precio, Estado, Categoria_id) VALUES ('A10', 'Lata Durazno', 'Durazno Enlatado', 49, 2430.00, 1, 1)
 
-EXECUTE InsertarProducto 5, 'c44', 'Arroz Danubio', 'Arroz Marca Danubio De 500grs', 45, 1500.00, 1, 4;
-EXECUTE InsertarProducto 6, 'c44', 'Arroz Danubio', 'Arroz Marca Danubio De 500grs', 45, 1500.00, 1, 4;
-EXECUTE InsertarProducto 7, 'c44', 'Arroz Danubio', 'Arroz Marca Danubio De 500grs', 45, 1500.00, 1, 4;
-EXECUTE InsertarProducto 8, 'c44', 'Arroz Danubio', 'Arroz Marca Danubio De 500grs', 45, 1500.00, 1, 4;
+EXECUTE InsertarProducto 'c44', 'Arroz Danubio', 'Arroz Marca Danubio De 500grs', 45, 1500.00, 1, 4;
+EXECUTE InsertarProducto 'c44', 'Arroz Danubio', 'Arroz Marca Danubio De 500grs', 45, 1500.00, 1, 4;
+EXECUTE InsertarProducto 'c44', 'Arroz Danubio', 'Arroz Marca Danubio De 500grs', 45, 1500.00, 1, 4;
+EXECUTE InsertarProducto 'c44', 'Arroz Danubio', 'Arroz Marca Danubio De 500grs', 45, 1500.00, 1, 4;
 
 --Realizar  update y delete sobre  algunos de los registros insertados  en esas tablas invocando a los procedimientos. 
 
