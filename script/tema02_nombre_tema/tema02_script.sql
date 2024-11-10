5ef11369c679d428bf38f91498639641accb2b8c
MarkJoestar <66145597+MarkJoestar@users.noreply.github.com>
2024-11-10 15:36:34 -0300

    Update tema02_script.sql

--- script/tema02_nombre_tema/tema02_script.sql
+++ script/tema02_nombre_tema/tema02_script.sql
@@ -1,32 +1,32 @@
 --Se crea un procedimiento almacenado para insertar nuevos productos.
-CREATE USER usuarioAdministrador WITH PASSWORD = 'password123';
-CREATE USER usuarioVisitante WITH PASSWORD = 'password456';
+CREATE USER Administrador WITH PASSWORD = 'password123';
+CREATE USER Vendedor WITH PASSWORD = 'password456';
 
 -- Asignar permisos al usuario administrador
-GRANT ALL ON SistemaCompras_Proyecto_BdD.* TO usuarioAdministrador;
+GRANT ALL ON SistemaCompras_Proyecto_BdD.* TO Administrador;
 
--- Asignar permisos al usuario visitante
-GRANT SELECT ON SistemaCompras_Proyecto_BdD.Productos TO usuarioVisitante;
-GRANT EXECUTE ON SistemaCompras_Proyecto_BdD.sp_InsertarProducto TO usuarioVisitante;
+-- Asignar permisos al usuario Vendedor
+GRANT SELECT ON SistemaCompras_Proyecto_BdD.Productos TO Vendedor;
+GRANT EXECUTE ON SistemaCompras_Proyecto_BdD./*procedimento*/ TO Vendedor;
 
 -- Intentar insertar un producto directamente con ambos usuarios
 -- Conectarse como usuarioAdministrador
 USE SistemaCompras_Proyecto_BdD;
 GO
-EXECUTE AS USER = 'usuarioAdministrador';
+EXECUTE AS USER = 'Administrador';
 /*Ejecutar procedimiento como administrador*/
 REVERT;
 
--- Conectarse como usuarioVisitante
+-- Conectarse como Vendedor
 USE tienda;
 GO
-EXECUTE AS USER = 'usuarioVisitante';
+EXECUTE AS USER = 'Vendedor';
 INSERT INTO Productos (Nombre, Precio) VALUES ('Otro Producto', 29.99);
 REVERT;
 
 -- Intentar insertar un producto usando el procedimiento almacenado con el usuario lector
 USE tienda;
 GO
-EXECUTE AS USER = 'usuarioVisitante';
-/*Ejecutar procedimiento como visitante*/
+EXECUTE AS USER = 'vendedor';
+/*Ejecutar procedimiento como vendedor*/
 REVERT;
